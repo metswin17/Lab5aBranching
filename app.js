322de9b289f32fbdeb84d8eaa383975bd35b59fa
Luis Lopez <metfanforever@gmail.com>
2026-02-12 15:02:38 -0800

    multiply pt2 solution

--- app.js
+++ app.js
@@ -52,12 +52,23 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
+function sumAndMultiply(a, b, c) {
 
+  let sumAB = sum(a, b)[0];
+  let totalSum = sum(sumAB, c)[0];
+
+  let multiplyAB = multiply(a, b)[0];
+  let totalProduct = multiply(multiplyAB, c)[0];
+
+  let sumMessage = a + " and " + b + " and " + c + " sum to " + totalSum + ".";
+  let productMessage = "The product of " + a + " and " + b + " and " + c + " is " + totalProduct + ".";
+
+  return [totalSum, totalProduct, sumMessage, productMessage];
 }
 
+
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
